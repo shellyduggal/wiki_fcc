b8a15729828b68cab5dcdfc2f44b167baafffba0
Shelly Duggal <shellduggal@gmail.com>
2017-02-08 13:44:18 -0600

    Configure API Call

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 console.log("hello");
 
+// function getRandom() {
+// 	$.ajax({
+// 		url: "https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrsearch=felix&prop=info&format=json",
+// 		dataType: "jsonp",
+// 		success: function(data) {
+// 			console.log(data);
+// 		}
+// 	}); //close ajax
+// }
+
+
 function getRandom() {
 	$.ajax({
-		url: "https://en.wikipedia.org/w/api.php?action=query&list=random",
-		dataType: "json",
+		url: "https://en.wikipedia.org/w/api.php?action=opensearch&search=felix&prop=info&inprop=url|displaytitle&format=json",
+		dataType: "jsonp",
 		success: function(data) {
 			console.log(data);
 		}
